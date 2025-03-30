71e997ab5164f985aa5d25d9c79c2673db1af58a
Rohan Sunuwar <melonbread2248@gmail.com>
2025-03-30 19:14:10 +0545

    changed code

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import pickle
 import requests
+import random
 
 movies_dict = pickle.load(open('data/movie_dict.pkl', 'rb'))
 similarity = pickle.load(open('data/similarity.pkl', 'rb'))
@@ -10,7 +11,7 @@ movies = pd.DataFrame(movies_dict)
 
 
 # recommends 5 movies
-def reccomend(movie):
+def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
@@ -34,15 +35,35 @@ def fetch_poster(movie_id):
     return full_path
 
 
-st.title('Movie Reccomendation System')
+def randomize():
+    for i in movies_dict:
+        numbers = movies.iloc[i[0]].id
+
+    reccomended_movies = []
+    reccomended_movies_posters = []
+
+    random_movies = random.sample(numbers, 5)
+
+    for i in movies_dict:
+        movie_id = movies.iloc[i[0]].id #id to fetch poster from TMDB API
+
+        reccomended_movies.append(random_movies[i[0]].title)
+        reccomended_movies_posters.append(fetch_poster(movie_id))
+    return reccomended_movies, reccomended_movies_posters
+
+
+st.title('Movie Recommendation System')
 
 selected_movie = st.selectbox(
-    'Enter Movie Name',
-    movies['title'].values
+    '🔍Enter Movie Name',
+    movies['title'].values,
 )
 
-if st.button('Reccomend'):
-    names, posters = reccomend(selected_movie)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie)
+
+    st.title('Recommended Movies')
 
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
@@ -58,5 +79,27 @@ if st.button('Reccomend'):
         st.text(names[3])
         st.image(posters[3])
     with col5:
-        st.text(names[4])
-        st.image(posters[4])
+            st.text(names[4])
+            st.image(posters[4])
+
+
+# if st.button('Randomizer🔀'):
+#     names, posters = randomize()
+#     st.title('Recommended Movies')
+
+#     col1, col2, col3, col4, col5 = st.columns(5)
+#     with col1:
+#         st.text(names[0])
+#         st.image(posters[0])
+#     with col2:
+#         st.text(names[1])
+#         st.image(posters[1])
+#     with col3:
+#         st.text(names[2])
+#         st.image(posters[2])
+#     with col4:
+#         st.text(names[3])
+#         st.image(posters[3])
+#     with col5:
+#         st.text(names[4])
+#         st.image(posters[4])
